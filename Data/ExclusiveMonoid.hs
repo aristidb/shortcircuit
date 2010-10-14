db9a9e3e5f51d0151141f12e9a9c0141beabd813
Aristid Breitkreuz <aristidb@googlemail.com>
2010-10-14 08:39:57 +0200

    simplify, remove monadLib dependency

--- Data/ExclusiveMonoid.hs
+++ Data/ExclusiveMonoid.hs
@@ -1,43 +1,37 @@
-{-# LANGUAGE MultiParamTypeClasses, FunctionalDependencies, FlexibleInstances, FlexibleContexts #-}
-
 module Data.ExclusiveMonoid
 where
   
 import Control.Monad
-import MonadLib
 import Data.Maybe
 import Data.Monoid
 
 class ExclusiveZero a where
     zero :: a
 
-class Monad m => ExclusiveMonoid a m | a -> m where
-    isValidM :: a -> m Bool
+class ExclusiveMonoid a where
+    isValid :: a -> Bool
     orElse :: a -> a -> a
 
-isValid :: ExclusiveMonoid a Id => a -> Bool
-isValid = runId . isValidM
-
-firstOf :: (ExclusiveZero a, ExclusiveMonoid a m) => [a] -> a
+firstOf :: (ExclusiveZero a, ExclusiveMonoid a) => [a] -> a
 firstOf = foldr orElse zero
 
 instance ExclusiveZero Bool where
     zero = False
 
-instance ExclusiveMonoid Bool Id where
-    isValidM = return . (== True)
+instance ExclusiveMonoid Bool where
+    isValid = (== True)
     orElse = (||)
 
 instance ExclusiveZero (Maybe a) where
     zero = Nothing
 
-instance ExclusiveMonoid (Maybe a) Id where
-    isValidM = return . isJust
+instance ExclusiveMonoid (Maybe a) where
+    isValid = isJust
     orElse = mplus
 
-instance ExclusiveMonoid (Either a b) Id where
-    isValidM (Right _) = return True
-    isValidM (Left _)  = return False
+instance ExclusiveMonoid (Either a b) where
+    isValid (Right _) = True
+    isValid (Left _)  = False
     orElse a@(Left _)    (Left _)  = a
     orElse   (Left _)  b@(Right _) = b
     orElse a@(Right _)   _         = a
@@ -47,38 +41,25 @@ newtype EitherSum a b = EitherSum { unEitherSum :: Either a b }
 instance Monoid a => ExclusiveZero (EitherSum a b) where
     zero = EitherSum $ Left mempty
 
-instance Monoid a => ExclusiveMonoid (EitherSum a b) Id where
-    isValidM (EitherSum x) = isValidM x
+instance Monoid a => ExclusiveMonoid (EitherSum a b) where
+    isValid (EitherSum x) = isValid x
     orElse (EitherSum x) (EitherSum y) = EitherSum $ orElse' x y
         where
           orElse'   (Left a)    (Left b)  = Left (a `mappend` b)
           orElse'   (Left _)  b@(Right _) = b
           orElse' a@(Right _)   _         = a
 
-newtype WrappedMonad m a = WrapMonad { unwrapMonad :: m a }
+monadZero :: (Monad m, ExclusiveZero a) => m a
+monadZero = return zero
 
-instance (Monad m, ExclusiveZero a) => ExclusiveZero (WrappedMonad m a) where
-    zero = WrapMonad $ return zero
+monadIsValid :: (Monad m, ExclusiveMonoid a) => m a -> m Bool
+monadIsValid = liftM isValid
 
-instance (Monad m, ExclusiveMonoid a Id) => ExclusiveMonoid (WrappedMonad m a) m where
-    isValidM = liftM isValid . unwrapMonad
-    orElse a b = WrapMonad $ do
-      x <- unwrapMonad a
-      case isValid x of
-        False -> unwrapMonad b
-        True  -> return x
-
-monadZero :: (Monad m, ExclusiveZero (WrappedMonad m a)) => m a
-monadZero = unwrapMonad zero
-
-monadIsValid :: (Monad m, ExclusiveMonoid (WrappedMonad m a) m) => m a -> m Bool
-monadIsValid = isValidM . WrapMonad
-
-monadOrElse :: (Monad m, ExclusiveMonoid (WrappedMonad m a) m) => m a -> m a -> m a
-monadOrElse a b = unwrapMonad $ orElse (WrapMonad a) (WrapMonad b)
+monadOrElse :: (Monad m, ExclusiveMonoid a) => m a -> m a -> m a
+monadOrElse a b = a >>= \x -> if isValid x then return x else b
 
 monadFirstOf :: (Monad m
-                , ExclusiveZero (WrappedMonad m a)
-                , ExclusiveMonoid (WrappedMonad m a) m) 
+                , ExclusiveZero a
+                , ExclusiveMonoid a) 
                 => [m a] -> m a
-monadFirstOf = unwrapMonad . firstOf . map WrapMonad
+monadFirstOf = foldr monadOrElse monadZero
